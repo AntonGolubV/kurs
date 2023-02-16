1886fce1e3b48ba24101825fcd0d20d4e9799c15
AntonGolubV <104014701+AntonGolubV@users.noreply.github.com>
2023-02-16 14:54:53 +0300

    Add files via upload

---
+++ Kurs/script.js
@@ -0,0 +1,7 @@
+/* let buttonLog = document.querySelector('.btn-outline-primary');
+buttonLog.addEventListener('click', function(){
+    let board = document.createElement('div');
+    board.classList.add('board');
+    document.body.append(board);
+    
+}) */
